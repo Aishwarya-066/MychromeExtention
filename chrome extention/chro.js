d0ffb0bdcd5b401c172bd233a0d7e0ebb5b469e9
Aishwarya-066 <111353857+Aishwarya-066@users.noreply.github.com>
2024-06-28 11:50:49 +0530

    Update chro.js

--- chrome extention/chro.js
+++ chrome extention/chro.js
@@ -1,67 +1,98 @@
- /*
-        function save(){
-        ------
-        }*/
-        let leads = []
+class LRUCache {
+    constructor(maxSize) {
+        this.maxSize = maxSize;
+        this.cache = new Map();
+    }
 
-        const inputEl = document.getElementById("input-el")
-        const inputBtn = document.getElementById("input-btn")
-        const ulEl = document.getElementById("ul-el")
-        const deleteBtn = document.getElementById("delete-btn")
-        const tabBtn = document.getElementById("tab-btn")
-       // const tab = [{url: "https://www.linkedin.com/in/per-harald-borgen/"}]
-       tabBtn.addEventListener("click", function(){    
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            leads.push(tabs[0].url)
-            localStorage.setItem("leads", JSON.stringify(leads))
-            render(leads)
-        })
-    })
-        const leadsFromLocalStorage = JSON.parse( localStorage.getItem("leads") )
-        if (leadsFromLocalStorage) {
-            leads = leadsFromLocalStorage
-            render(leads)
+    get(key) {
+        if (!this.cache.has(key)) return null;
+        const value = this.cache.get(key);
+        // Move to the end to show that it was recently used
+        this.cache.delete(key);
+        this.cache.set(key, value);
+        return value;
+    }
+
+    set(key, value) {
+        if (this.cache.has(key)) {
+            this.cache.delete(key);
+        } else if (this.cache.size === this.maxSize) {
+            // Remove the first (least recently used) item
+            this.cache.delete(this.cache.keys().next().value);
         }
+        this.cache.set(key, value);
+    }
 
-        deleteBtn.addEventListener("dblclick", function(){
-            localStorage.clear()
-            leads = []
-            render(leads)
-        })
-        inputBtn.addEventListener("click",function(){
-            leads.push(inputEl.value)
-            inputEl.value = ""
-            localStorage.setItem("leads", JSON.stringify(leads) )
-            render(leads)
-        })
+    values() {
+        return Array.from(this.cache.values());
+    }
 
-        function render(myleads){
-            let listItems = ""
-        for (let i = 0; i < myleads.length; i++) {
- //  listItems += "<li><a target='_blank' href='" + leads[i] + "'></a></li>"
-   /*const li = document.createElement("li")
-    li.textContent = leads[i]
-    ulEl.append(li)*/
-    //template string
-    listItems += `
-            <li>
-                <a target="_blank" href="${myleads[i]}">  ${myleads[i]} </a>
-            </li>
-    `
+    reset() {
+        this.cache.clear();
+    }
 }
-ulEl.innerHTML = listItems
-        }
 
-        /*
-        let myLeads = `["www.awesomelead.com"]`
+// Use the LRU cache class in your script
+let leads = [];
 
-// 1. Turn the myLeads string into an array
-myLeads = JSON.parse(myLeads)
-// 2. Push a new value to the array
-myLeads.push("www.lead2.com")
-// 3. Turn the array into a string again
-myLeads = JSON.stringify(myLeads)
-// 4. Console.log the string using typeof to verify that it's a string
-console.log(typeof myLeads)
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
-        */
+// Create an LRU cache instance with a max size of 5 items
+const cache = new LRUCache(5);
+
+// Load leads from localStorage
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("leads"));
+if (leadsFromLocalStorage) {
+    leadsFromLocalStorage.forEach(lead => cache.set(lead, lead));
+    render(cache.values());
+}
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        addLead(tabs[0].url);
+    });
+});
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    cache.reset();
+    render([]);
+});
+
+inputBtn.addEventListener("click", function() {
+    addLead(inputEl.value);
+    inputEl.value = "";
+});
+
+function addLead(lead) {
+    if (!cache.get(lead)) {
+        cache.set(lead, lead);
+    }
+    localStorage.setItem("leads", JSON.stringify(cache.values()));
+    render(cache.values());
+}
+
+function render(myleads) {
+    let listItems = "";
+    for (let i = 0; i < myleads.length; i++) {
+        listItems += `
+            <li>
+                <a href="#" onclick="linkClickHandler('${myleads[i]}')">${myleads[i]}</a>
+            </li>
+        `;
+    }
+    ulEl.innerHTML = listItems;
+}
+
+function linkClickHandler(link) {
+    // Update the cache when a link is clicked
+    cache.get(link);
+    cache.set(link, link);
+    localStorage.setItem("leads", JSON.stringify(cache.values()));
+    render(cache.values());
+    window.open(link, '_blank');
+}
